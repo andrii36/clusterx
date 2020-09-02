721fc97f4dab374f5bd120b53d93b0a43b0d5932
unknown <vovchenkoandrii@gmail.com>
2020-09-01 21:51:24 -0700

    Updates

---
+++ precache-manifest.5493be1c6eb102a195414dbffc1ffcce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48dc56d4abc6a10bf3436d7c46c3f03c",
+    "url": "/clusterx/index.html"
+  },
+  {
+    "revision": "3d59f3239589f83bba87",
+    "url": "/clusterx/static/css/main.780c9044.chunk.css"
+  },
+  {
+    "revision": "d0126e0cc073c4178611",
+    "url": "/clusterx/static/js/2.37d226ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clusterx/static/js/2.37d226ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d59f3239589f83bba87",
+    "url": "/clusterx/static/js/main.04923ad3.chunk.js"
+  },
+  {
+    "revision": "e2ab55d85533344471a0",
+    "url": "/clusterx/static/js/runtime-main.ded16c5f.js"
+  },
+  {
+    "revision": "e0c6e41b2de8d8fde71f98265fe543b0",
+    "url": "/clusterx/static/media/logo-clusterx.e0c6e41b.png"
+  }
+]);
